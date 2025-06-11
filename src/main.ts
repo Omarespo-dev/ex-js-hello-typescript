2013d43cfc51ce4168eda9dc607a9f324e902524
Omarespo-dev <omaresposito21@gmail.com>
2025-06-11 23:45:26 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let messaggio = "ciao"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+//toFixed e una stringa mettere come tipo number 
+let number = Number(23 .toFixed(2))
+const totale = number + 10
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+console.log(totale);
+
